e61ed3f76e3dd7ff678c0869722ab7da3e7d3fce
kira vivian <rkira.vivian@gmail.com>
2025-11-10 16:19:26 +0100

    Initial commit

---
+++ deploymentfile.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+df = pd.read_csv("FuelConsumptionCo2.csv")
+
+cdf = df[['ENGINESIZE','CYLINDERS','FUELCONSUMPTION_COMB','CO2EMISSIONS']]
+cdf.head(9)
+viz = cdf[['CYLINDERS','ENGINESIZE','CO2EMISSIONS','FUELCONSUMPTION_COMB']]
+viz.hist()
+plt.show()
